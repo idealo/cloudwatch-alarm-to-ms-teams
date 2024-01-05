0d572374d5bb79dcb785b1ba0c59bb5227c3899f
Jan Guntowski <jan.guntowski@idealo.de>
2024-01-05 10:34:05 +0100

    fix: solve issues with CW Alarm URLs, add development stack, update to node 20

---
+++ dev/src/cdk.ts
@@ -0,0 +1,19 @@
+import {App} from 'aws-cdk-lib';
+import {DevelopmentStack} from './development-stack';
+
+const app = new App();
+new DevelopmentStack(app, 'CloudWatchAlarmToMsTeamsDevelopmentStack', getWebhookUrl(app), {
+    env: {
+        region: process.env.CDK_DEFAULT_REGION,
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+    },
+})
+
+
+function getWebhookUrl(app: App): string {
+    const url = app.node.tryGetContext('webhook-url');
+    if (!url) {
+        throw new Error('Webhook url not specified. Usage: npm run cdk-deploy -- -c webhook-url=https://you-webhook-url')
+    }
+    return url
+}
